7316632db986159776e412f1d71301ef16c330ce
claudiobailon <c.bailonschubert@gmail.com>
2020-07-13 15:49:03 -0700

    Finished location

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+require('dotenv').config();
+
+const cors = require('cors');
+app.use(cors());
+
+const PORT = process.env.PORT || 3001;
+
+app.get('/location', (request, response) => {
+  let city = request.query.city;
+  let geoData = require('./data/location.json');
+
+  const obj = new Location(city, geoData);
+  response.send(obj);
+});
+
+function Location(location, geoData){
+  this.search_query = location;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+})
