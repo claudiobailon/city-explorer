f7a2c156b0882bff2362641918ddef310dc3602a
claudiobailon <c.bailonschubert@gmail.com>
2020-07-14 20:39:54 -0700

    Added path for trails

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ function handleWeather(request, response) {
     city: request.query.search_query,
     days:8
   }
-
   superagent.get(url)
     .query(queryParamaters)//.query is a built in method
     .then(dataFromSuperAgent => {
@@ -66,6 +65,37 @@ function handleWeather(request, response) {
 
 }
 
+app.get('/trails', handleTrails);
+
+function handleTrails(request,response){
+  let url = `https://www.hikingproject.com/data/get-trails`;
+  let queryParamaters ={
+    lat: request.query.latitude,
+    lon: request.query.longitude,
+    key: process.env.TRAIL_API_KEY,
+  }
+  superagent.get(url)
+    .query(queryParamaters)
+    .then(dataFromSuperAgent =>{
+      const trailsArray = dataFromSuperAgent.body.data.map(hike => {
+        return new Trail(hike);
+      })
+    })
+}
+
+// [
+//   {
+//     "name": "Rattlesnake Ledge",
+//     "location": "Riverbend, Washington",
+//     "length": "4.3",
+//     "stars": "4.4",
+//     "star_votes": "84",
+//     "summary": "An extremely popular out-and-back hike to the viewpoint on Rattlesnake Ledge.",
+//     "trail_url": "https://www.hikingproject.com/trail/7021679/rattlesnake-ledge",
+//     "conditions": "Dry: The trail is clearly marked and well maintained.",
+//     "condition_date": "2018-07-21",
+//     "condition_time": "0:00:00 "
+//   },
 //========================Contructor Funtions===================
 function Location(location, geoData){
   this.search_query = location;
